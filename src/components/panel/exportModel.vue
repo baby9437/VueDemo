4ddc5169108d0851df3e62fa4263324011eeffe2
baby9437 <943711466@qq.com>
2018-05-30 16:05:32 +0800

    添加组件terrain，navigate，scale

---
+++ src/components/panel/exportModel.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <div>
+        <img style="width: 25px;" src="../../../static/img/png64/325shuipingArea.png" alt="">
+        多边形选择模型范围
+      </div>
+      <el-button type="primary">导出</el-button>
+      <div style="color: red">注：本功能仅可导出规划模型。</div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'exportModel',
+        data() {
+            return {}
+        },
+        watch: {},
+        computed: {},
+        methods: {},
+        mounted() {
+
+        },
+        components: {},
+    }
+</script>
+<style scoped>
+
+</style>
