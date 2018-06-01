457c30c01f1f14b4f0b81f9598466b06e3e36ba7
baby9437 <943711466@qq.com>
2018-06-01 09:26:51 +0800

    added schema.vue

---
+++ src/components/panel/schema.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: '',
+        data() {
+            return {}
+        },
+        watch: {},
+        computed: {},
+        methods: {},
+        mounted() {
+
+        },
+        components: {},
+    }
+</script>
+<style scoped>
+
+</style>
