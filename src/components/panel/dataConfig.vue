c6157398234de0e8eeab7e6bf683ce460b4eddef
baby9437 <943711466@qq.com>
2018-06-06 14:49:31 +0800

    added dataconfig and userpermission

--- src/components/panel/dataConfig.vue
+++ src/components/panel/dataConfig.vue
@@ -1,44 +1,106 @@
 <template>
-    <div class="panelBody">
-        <el-tabs v-model="activeTab" type="card" @tab-click="">
-          <el-tab-pane label="数据库配置" name="">
+<div>
+  <div class="title">用户权限</div>
+  <div class="panelBody" style="width: 500px;">
+    <el-tabs v-model="activeTab" type="card" @tab-click="">
+      <el-tab-pane label="数据库配置" name="">
+        <div style="text-align: center;">
+          <div style="display: inline-block;">
+            <div>三维现状模型数据：</div>
+            <div>
+              <el-form ref="form_current" :model="form_current" label-width="80px">
+                <el-form-item label="Server IP:">
+                  <el-input v-model="form_current.ip"></el-input>
+                </el-form-item>
+                <el-form-item label="Port：">
+                  <el-input v-model="form_current.port"></el-input>
+                </el-form-item>
+                <el-form-item label="Service：">
+                  <el-input v-model="form_current.service"></el-input>
+                </el-form-item>
+                <el-form-item label="Password：">
+                  <el-input v-model="form_current.psw"></el-input>
+                </el-form-item>
+              </el-form>
+            </div>
+          </div>
+          <div style="display: inline-block;">
+            <div>三维现状地形数据：</div>
+            <div>
+              <el-form ref="form_terrain" :model="form_current" label-width="80px">
+                <el-form-item label="Server IP:">
+                  <el-input v-model="form_terrain.ip"></el-input>
+                </el-form-item>
+                <el-form-item label="Port：">
+                  <el-input v-model="form_terrain.port"></el-input>
+                </el-form-item>
+                <el-form-item label="数据源：">
+                  <el-input v-model="form_terrain.dataSource"></el-input>
+                </el-form-item>
+                <el-form-item label="数据集：">
+                  <el-input v-model="form_terrain.dataset"></el-input>
+                </el-form-item>
+              </el-form>
+            </div>
+          </div>
+          <div style="display: inline-block;">
+            <div>三维规划模型数据：</div>
+            <div>
+              <el-form ref="form_plan" :model="form_plan" label-width="80px">
+                <el-form-item label="Server IP:">
+                  <el-input v-model="form_plan.ip"></el-input>
+                </el-form-item>
+                <el-form-item label="Port：">
+                  <el-input v-model="form_plan.port"></el-input>
+                </el-form-item>
+                <el-form-item label="Service：">
+                  <el-input v-model="form_plan.service"></el-input>
+                </el-form-item>
+                <el-form-item label="Password：">
+                  <el-input v-model="form_plan.psw"></el-input>
+                </el-form-item>
+              </el-form>
+            </div>
+          </div>
+        </div>
 
-            1
-          </el-tab-pane>
-          <el-tab-pane label="项目类型表" name="">
+      </el-tab-pane>
+      <el-tab-pane label="项目类型表" name="">
 
-            2
-          </el-tab-pane>
-          <el-tab-pane label="项目状态表" name="">
-            3
+        2
+      </el-tab-pane>
+      <el-tab-pane label="项目状态表" name="">
+        3
 
-          </el-tab-pane>
-          <el-tab-pane label="方案结构表" name="">
-
-
-          </el-tab-pane>
-          <el-tab-pane label="对象类型表" name="">
+      </el-tab-pane>
+      <el-tab-pane label="方案结构表" name="">
 
 
-          </el-tab-pane>
-          <el-tab-pane label="数据来源表" name="">
+      </el-tab-pane>
+      <el-tab-pane label="对象类型表" name="">
 
 
-          </el-tab-pane>
-          <el-tab-pane label="规划一张图系统" name="">
+      </el-tab-pane>
+      <el-tab-pane label="数据来源表" name="">
 
 
-          </el-tab-pane>
-          <el-tab-pane label="规划综合应用平台" name="">
+      </el-tab-pane>
+      <el-tab-pane label="规划一张图系统" name="">
 
 
-          </el-tab-pane>
-          <el-tab-pane label="多规合一信息联动平台" name="">
+      </el-tab-pane>
+      <el-tab-pane label="规划综合应用平台" name="">
 
 
-          </el-tab-pane>
-        </el-tabs>
-    </div>
+      </el-tab-pane>
+      <el-tab-pane label="多规合一信息联动平台" name="">
+
+
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -47,7 +109,10 @@
         name: '',
         data() {
             return {
-              activeTab:''
+              activeTab:'',
+              form_current:{ip:'',port:'',service:'',psw:''},
+              form_terrain:{ip:'',port:'',dataSource:'',dataset:''},
+              form_plan:{ip:'',port:'',service:'',psw:''}
             }
         },
         watch: {},
--- src/components/panel/userPermission.vue
+++ src/components/panel/userPermission.vue
@@ -1,11 +1,46 @@
 <template>
   <div>
-      <div class="panelBody" style="width: 500px">
+    <div class="title">用户权限</div>
+      <div class="panelBody" style="width: 400px">
         <el-tabs v-model="activeTab" type="card" @tab-click="">
-          <el-tab-pane label="角色表" name="first">角色表</el-tab-pane>
-          <el-tab-pane label="部门表" name="second">部门表</el-tab-pane>
-          <el-tab-pane label="用户表" name="third">用户表</el-tab-pane>
-          <el-tab-pane label="系统日志表" name="fourth">系统日志表</el-tab-pane>
+          <el-tab-pane label="角色表" name="first">
+            <el-table :data="tb_role" style="width: 100%">
+              <el-table-column prop="id" label="ID" width="100"></el-table-column>
+              <el-table-column prop="name" label="名称" width="100"></el-table-column>
+              <el-table-column prop="desc" label="描述" width="100"></el-table-column>
+            </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="部门表" name="second">
+            <el-table :data="tb_depart" style="width: 100%">
+              <el-table-column prop="id" label="ID" width="100"></el-table-column>
+              <el-table-column prop="name" label="名称" width="100"></el-table-column>
+              <el-table-column prop="desc" label="描述" width="100"></el-table-column>
+            </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="用户表" name="third">
+            <el-table :data="tb_user" style="width: 100%">
+              <el-table-column prop="id" label="ID" width="100"></el-table-column>
+              <el-table-column prop="name" label="名称" width="100"></el-table-column>
+              <el-table-column prop="cnName" label="中文名" width="100"></el-table-column>
+              <el-table-column prop="password" label="密码" width="100"></el-table-column>
+              <el-table-column prop="createTime" label="创建时间" width="100"></el-table-column>
+              <el-table-column prop="lastLoginTime" label="上次登录时间" width="100"></el-table-column>
+              <el-table-column prop="depart" label="部门" width="100"></el-table-column>
+              <el-table-column prop="role" label="角色" width="100"></el-table-column>
+            </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="系统日志表" name="fourth">
+            <el-table :data="tb_log" style="width: 100%">
+              <el-table-column prop="id" label="日志ID" width="100"></el-table-column>
+              <el-table-column prop="userId" label="用户ID" width="100"></el-table-column>
+              <el-table-column prop="name" label="用户名" width="100"></el-table-column>
+              <el-table-column prop="cnName" label="中文名" width="100"></el-table-column>
+              <el-table-column prop="pc" label="电脑" width="100"></el-table-column>
+              <el-table-column prop="ip" label="IP" width="100"></el-table-column>
+              <el-table-column prop="time" label="时间" width="100"></el-table-column>
+              <el-table-column prop="desc" label="操作内容" width="100"></el-table-column>
+            </el-table>
+          </el-tab-pane>
         </el-tabs>
       </div>
   </div>
@@ -18,7 +53,10 @@
     data() {
       return {
         activeTab:'',
-        dialogVisible:true
+        tb_role:[],
+        tb_depart:[],
+        tb_user:[],
+        tb_log:[]
       }
     },
     watch: {},
